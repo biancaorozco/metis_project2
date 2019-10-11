67be2784cc0fd9ee7527f1ed4f6a9158262ac9fc
Bianca Orozco <biorozco3@gmail.com>
2019-10-10 19:49:18 -0700

    finished airlines analysis code

--- xcode/airlines_analysis.py
+++ xcode/airlines_analysis.py
@@ -13,8 +13,11 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import Ridge 
+
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import FeatureUnion
 from sklearn.pipeline import make_pipeline
 from sklearn_pandas import DataFrameMapper
@@ -64,12 +67,6 @@ df.isna().sum()
 
 ## Converting Flight ID to Integers
 df['id_flight'] = pd.to_numeric(df.id_flight, downcast='signed')
-
-## Replacing Number of Stops
-df['number_stops'].replace('(Nonstop)', value=int('0'), regex=True, inplace=True)
-df['number_stops'].replace('(1 stop)', value=int('1'), regex=True, inplace=True)
-df['number_stops'].replace('(2 stop)', value=int('2'), regex=True, inplace=True)
-df['number_stops'].replace('(3 stop)', value=int('3'), regex=True, inplace=True)
             
 ## Removing $ and , from Prices and Convert to Numeric
 df['prices'].replace('\$', value='', regex=True, inplace=True)
@@ -114,61 +111,59 @@ gb = df.groupby(['departure_airport', 'arrival_airport'])
 aggreg = gb.agg({'number_stops':['count']})
 aggreg
 
+## Checking Distribution of Prices
+plt.hist(df['prices'])
 
-############### Features ###############
+############### 1.Features ###############
 X = df.loc[:,['departure_time', 'arrival_time', 'airline', 'duration', 'number_stops', 'departure_airport', 'arrival_airport', 'date']]
 y = df['prices']
 
+## Prices are skewed left so take log to transform into a Gaussian Dist.
+log_y = np.log(y)
+plt.hist(log_y)
 
-############### Feature Engineering  ###############
-## Interaction Effects
-X2 = X.copy()
-# The number of stops effects the duration of the flight
-X2['dur_x_stops'] = X2['duration'] * X2['number_stops']
-
-
-############### Dummy Variables ###############
+############### 2.Dummy Variables ###############
 ## Dummy Variables for Airlines, Departure Airport, Arrival Airport, and Date
 print(X['airline'].nunique(), 'airlines') # 10 airlines
 print(X['departure_airport'].nunique(), 'departure airports')
-print(X['arrival_airport'].nunique(), 'arrival airports\n')
-X2 = pd.get_dummies(X)
-X2.info()
+print(X['arrival_airport'].nunique(), 'arrival airports')
+print(X['number_stops'].nunique(), 'number of stops\n')
+X = pd.get_dummies(X)
+X.info()
 
 
-############### Split Data ###############
-# X_train, X_test, y_train, y_test = train_test_split(X2, y, test_size=.2, random_state=12)
+############### 3.Split Data ###############
+## Splitting Train, Validate, Test Rows
+X, X_test, y_log, y_test = train_test_split(X, log_y, test_size=.2, random_state=10)
+X_train, X_val, y_train, y_val = train_test_split(X, log_y, test_size=.25, random_state=33)
 
-X, X_test, y, y_test = train_test_split(X, y, test_size=.2, random_state=10)
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=.25, random_state=33)
 
-
-############### Train/Validation/Test ###############
-## First Model
+############### Scaling & Modeling ###############
+# Model 1
 lm = LinearRegression()
 
-#Feature scaling for train, val, and test to run ridge model on each
+# Feature Scaling
 scaler = StandardScaler()
 
 X_train_scaled = scaler.fit_transform(X_train.values)
 X_val_scaled = scaler.transform(X_val.values)
 X_test_scaled = scaler.transform(X_test.values)
 
-## Second Model
+# Model 2
 lm_reg = Ridge(alpha=1)
 
-#Feature transforms for train, val, and test to run poly model on each
+# Feature Transform
 poly = PolynomialFeatures(degree=2) 
 
 X_train_poly = poly.fit_transform(X_train.values)
 X_val_poly = poly.transform(X_val.values)
 X_test_poly = poly.transform(X_test.values)
 
-## Third Model
+# Model 3
 lm_poly = LinearRegression()
 
 
-###############  ###############
+############### Choose Model ###############
 
 lm.fit(X_train, y_train)
 print(f'Linear Regression val R^2: {lm.score(X_val, y_val):.3f}')
@@ -179,24 +174,17 @@ print(f'Ridge Regression val R^2: {lm_reg.score(X_val_scaled, y_val):.3f}')
 lm_poly.fit(X_train_poly, y_train)
 print(f'Degree 2 polynomial regression val R^2: {lm_poly.score(X_val_poly, y_val):.3f}')
 
-############### K-Folds ###############
-lm = LinearRegression()
-kf = KFold(n_splits=5, # folds
-            shuffle=True,  
-            random_state=33)# arbitrary, but important
-kf_scores = cross_val_score(lm, X_train, y_train, # estimator, features, target
-                cv = kf, # 5 folds
-                scoring='r2') # scoring metric
-print(np.mean(kf_scores))
 
+############### Test ###############
+lm.fit(X,log_y)
+print(f'Linear Regression test R^2: {lm.score(X_test, y_test):.3f}')
+pred_train = lm.predict(X_train)
+pred_test = lm.predict(X_test)
 
+############### Actual vs Predicted ###############
+plt.scatter(pred_test, y_test, alpha=.2)
+# plt.plot(np.linspace(4,8,1), np.linspace(4,8,1))
+plt.title('Actual vs. Predicted Airline Prices')
+plt.xlabel('Predicted Airline Prices')
+plt.ylabel('Actual Airline Prices');
 
-
-
-
-posTransform = make_pipeline(FunctionTransformer(np.log), StandardScaler())
-
-mapper = DataFrameMapper([
-        (['arrival_time', 'departure_time'], posTransform),
-        (['duration'])
-])
